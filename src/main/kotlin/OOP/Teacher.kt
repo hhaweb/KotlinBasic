506763986ea3e1b79a25cbfe7a277db48f171dfe
Hein Htet Aung <heinhtetaung.mdy24@gmail.com>
2022-08-05 01:04:51 +0630

    Test Commit

--- src/main/kotlin/OOP/Teacher.kt
+++ src/main/kotlin/OOP/Teacher.kt
@@ -3,6 +3,6 @@ package OOP
 class Teacher: Person("Hein Htet","Aung",30), DoSomething {
     override val id: String = "Teacher $fullName-$age"
     override fun doSomething() {
-        println("I will teach you");
+        println("I will teach you"); // test coment
     }
 }
