20ab0a36ea33aff9c68101fb602317b3168e642d
Hein Htet Aung <heinhtetaung.mdy24@gmail.com>
2022-08-05 01:01:28 +0630

    test commit

--- src/main/kotlin/OOP/DoSomething.kt
+++ src/main/kotlin/OOP/DoSomething.kt
@@ -2,4 +2,5 @@ package OOP
 
 interface DoSomething {
     fun doSomething();
+    // test comment
 }
--- src/main/kotlin/OOP/Person.kt
+++ src/main/kotlin/OOP/Person.kt
@@ -1,7 +1,5 @@
 package OOP
 
-import java.util.*
-
 open class Person constructor(var firstName: String ,var lastName: String,var age: Int) {
     val upperCaseFullName: String = firstName.uppercase() + " " + lastName.uppercase()
     val fullName: String
